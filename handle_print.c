3e9c227d78023b9d077db257426633c8a6639182
Seif-01 <sifreyassin@gmail.com>
2023-06-27 15:09:12 -0700

    8

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * handle_print - Prints an argument based on its type
+ * handle_print_modified - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
+ * @ind: Ind.
  * @list: List of arguments to be printed.
- * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * @flags: Calculates active flags.
+ * @width: Width value.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ *
+ * Return: 1 or 2.
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int handle_print_modified(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int s, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +24,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (s = 0; fmt_types[s].fmt != '\0'; s++)
+		if (fmt[*ind] == fmt_types[s].fmt)
+			return (fmt_types[s].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[s].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
@@ -48,3 +50,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	return (printed_chars);
 }
 
+int main()
+{
+	// Your main function code here
+	
+	return 0;
+}
