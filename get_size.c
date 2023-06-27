59853a25fb65abf5d65be55b436399e75429b8a9
Seif-01 <sifreyassin@gmail.com>
2023-06-27 01:05:28 -0700

    8

--- get_size.c
+++ get_size.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_size - Calculates the size to cast the argument.
+ * @frmt: Formatted string in which to print the arguments.
+ * @s: List of arguments to be printed.
  *
  * Return: Precision.
  */
-int get_size(const char *format, int *i)
+int get_size(const char *frmt, int *s)
 {
-	int curr_i = *i + 1;
-	int size = 0;
+    int cur_i = *s + 1;
+    int sz = 0;
 
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
+    if (frmt[cur_i] == 'l')
+        sz = S_LONG;
+    else if (frmt[cur_i] == 'h')
+        sz = S_SHORT;
 
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
+    if (sz == 0)
+        *s = cur_i - 1;
+    else
+        *s = cur_i;
 
-	return (size);
+    return sz;
 }
-
-
