a8c04eb8506486c0be7b5f04543d0693f30ad94c
Seif-01 <sifreyassin@gmail.com>
2023-06-27 14:05:37 -0700

    5

--- get_flags.c
+++ get_flags.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags -  calculates active flags
+ * @format:  formatted string in which to print the arguments
+ * @i:   Take a parameter.
+ * Return:  flags representing the active flags.
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
-	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+    /* - + 0 # ' ' */
+    /* 1 2 4 8  16 */
+    int x, curr_i;
+    int flags = 0;
+    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
+    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+    {
+        for (x = 0; FLAGS_CH[x] != '\0'; x++)
+            if (format[curr_i] == FLAGS_CH[x])
+            {
+                flags |= FLAGS_ARR[x];
+                break;
+            }
 
-		if (FLAGS_CH[j] == 0)
-			break;
-	}
+        if (FLAGS_CH[x] == 0)
+            break;
+    }
 
-	*i = curr_i - 1;
+    *i = curr_i - 1;
 
-	return (flags);
+    return (flags);
 }
-
