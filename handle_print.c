bb2344aa5e597bfc0cb50c501f0dff83822d73b8
Seif-01 <sifreyassin@gmail.com>
2023-06-27 15:34:46 -0700

    9

--- handle_print.c
+++ handle_print.c
@@ -1,49 +1,57 @@
 #include "main.h"
+
 /**
-* handle_print - Prints an argument based on its type
-* @fmt: Formatted string in which to print the arguments.
-* @list: List of arguments to be printed.
-* @ind: ind.
-* @flags: Calculates active flags
-* @width: get width.
-* @precision: Precision specification
-* @buffer: Buffer array to handle print.
-* @size: Size specifier
-* Return: 1 or 2;
-*/
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+ * handle_print_modified - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @ind: ind.
+ * @list: List of arguments to be printed.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handle_print_modified(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-int i, unknow_len = 0, printed_chars = -1;
-fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'i', print_int}, {'d', print_int}, {'b', print_binary},
-{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-};
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-if (fmt[*ind] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	int s, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (s = 0; fmt_types[s].fmt != '\0'; s++)
+		if (fmt[*ind] == fmt_types[s].fmt)
+			return (fmt_types[s].fn(list, buffer, flags, width, precision, size));
 
-if (fmt_types[i].fmt == '\0')
-{
-if (fmt[*ind] == '\0')
-return (-1);
-unknow_len += write(1, "%%", 1);
-if (fmt[*ind - 1] == ' ')
-unknow_len += write(1, " ", 1);
-else if (width)
-{
---(*ind);
-while (fmt[*ind] != ' ' && fmt[*ind] != '%')
---(*ind);
-if (fmt[*ind] == ' ')
---(*ind);
-return (1);
+	if (fmt_types[s].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
 }
-unknow_len += write(1, &fmt[*ind], 1);
-return (unknow_len);
-}
-return (printed_chars);
+
+int main()
+{
+	// Your main function code here
+	
+	return 0;
 }
