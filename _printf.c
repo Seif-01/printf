0f89c99dbc0bd7c1687ac333c995b0294de3417b
Seif-01 <sifreyassin@gmail.com>
2023-06-27 00:00:45 -0700

    2

--- _printf.c
+++ _printf.c
@@ -10,60 +10,57 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    int s, printed = 0, printed_chars = 0;
+    int flags, width, precision, size, buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(list, format);
+    va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    for (s = 0; format && format[s] != '\0'; s++)
+    {
+        if (format[s] != '%')
+        {
+            buffer[buff_ind++] = format[s];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            printed_chars++;
+        }
+        else
+        {
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &s);
+            width = get_width(format, &s, list);
+            precision = get_precision(format, &s, list);
+            size = get_size(format, &s);
+            ++s;
+            printed = handle_print(format, &s, list, buffer,
+                                   flags, width, precision, size);
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
+        }
+    }
 
-	print_buffer(buffer, &buff_ind);
+    print_buffer(buffer, &buff_ind);
+    va_end(list);
 
-	va_end(list);
-
-	return (printed_chars);
+    return (printed_chars);
 }
 
 /**
 * print_buffer - prints the contents of the buffer if it exists
 * @buffer: Array of characters
-* @buff_ind: Index at which to add next character, represents the length.
+* @buff_ind: Index at which to add the next character, represents the length.
 */
 
 void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+    if (*buff_ind > 0)
+        write(1, &buffer[0], *buff_ind);
+    *buff_ind = 0;
 }
 
