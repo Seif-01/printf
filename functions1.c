4b8cf668702b43cde518f5eca7d5652b75f3c570
Seif-01 <sifreyassin@gmail.com>
2023-06-27 06:17:24 -0700

    3

--- functions1.c
+++ functions1.c
@@ -1,40 +1,5 @@
 #include "main.h"
 
-/************************* PRINT UNSIGNED NUMBER *************************/
-/**
-* print_unsigned - Prints an unsigned number
-* @types: List a of arguments
-* @buffer: Buffer array to handle print
-* @width: get width
-* @precision: Precision specification
-* @flags:  Calculates active flags
-* @size: Size specifier
-* Return: Number of chars printed.
-*/
-int print_unsigned(va_list types, char buffer[],
-int flags, int width, int precision, int size)
-{
-int i = BUFF_SIZE - 2;
-unsigned long int num = va_arg(types, unsigned long int);
-
-num = convert_size_unsgnd(num, size);
-
-if (num == 0)
-buffer[i--] = '0';
-
-buffer[BUFF_SIZE - 1] = '\0';
-
-while (num > 0)
-{
-buffer[i--] = (num % 10) + '0';
-num /= 10;
-}
-
-i++;
-
-return (write_unsgnd(0, i, buffer, flags, width, precision, size));
-}
-
 /************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
 /**
 * print_octal - Prints an unsigned number in octal notation
@@ -113,6 +78,41 @@ return (print_hexa(types, "0123456789ABCDEF", buffer,
 flags, 'X', width, precision, size));
 }
 
+/************************* PRINT UNSIGNED NUMBER *************************/
+/**
+* print_unsigned - Prints an unsigned number
+* @types: List a of arguments
+* @buffer: Buffer array to handle print
+* @width: get width
+* @precision: Precision specification
+* @flags:  Calculates active flags
+* @size: Size specifier
+* Return: Number of chars printed.
+*/
+int print_unsigned(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+
+num = convert_size_unsgnd(num, size);
+
+if (num == 0)
+buffer[i--] = '0';
+
+buffer[BUFF_SIZE - 1] = '\0';
+
+while (num > 0)
+{
+buffer[i--] = (num % 10) + '0';
+num /= 10;
+}
+
+i++;
+
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+
 /************** PRINT HEXX NUM IN LOWER OR UPPER **************/
 /**
 * print_hexa - Prints a hexadecimal number in lower or upper
@@ -158,3 +158,4 @@ i++;
 
 return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+
